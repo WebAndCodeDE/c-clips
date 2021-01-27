2d750ddb7f99872b07fcd6f3b02ce3608535435c
Andy <lockdown20@outlook.de>
2021-01-27 13:32:10 +0100

    ...

--- pointer-2.c
+++ pointer-2.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h> // Wird für die abs()-Funktion benötigt
 
 void update(int *a,int *b) {
     // Complete this function
-    int diff = 0;
-    if(*a >= *b){
-        diff = *a - *b;
-    } else {
-        diff = *b - *a;
-    }
+    int x, y;
+    x = *a + *b;
+    y = *a - *b;
+    *a = x;
+    *b = abs(y);
 }
 
 int main() {
     int a, b;
     int *pa = &a, *pb = &b;
-
+    printf("Bitte zwei Zahlen eingeben: ");
     scanf("%d %d", &a, &b);
     update(pa, pb);
     printf("%d\n%d", a, b);
-
     return 0;
 }
